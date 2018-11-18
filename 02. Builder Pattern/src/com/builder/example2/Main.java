d0ffbefc92d99b1444aae8019b7d5186acd5a8e4
Xemosoft <xemsoftuser123>
2018-11-18 21:30:51 +0600

    Initial Commit

--- 02. Builder Pattern/src/com/builder/example2/Main.java
+++ 02. Builder Pattern/src/com/builder/example2/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
         User user = new User.Builder()
                 .withId(1L)
-                .withName("Partha")
-                .withEmail("partha@gmail.com")
+                .withName("bob")
+                .withEmail("bob@gmail.com")
                 .withPassword("123456")
                 .withPhone("25193012312312")
                 .build();
